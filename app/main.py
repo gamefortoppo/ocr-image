4acbb6d411d59c46ecd7d5881706f20fb7b15e9c
gamefortoppo <gamefortoppo01@gmail.com>
2025-12-17 16:24:06 +0700

    Update main.py
    change to tesseract

--- app/main.py
+++ app/main.py
@@ -1,31 +1,18 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, UploadFile, File
+from PIL import Image
+import io
+
 from app.ocr_service import run_ocr
 
-app = FastAPI(title="PaddleOCR Backend")
-
-# Allow website outside call this BE
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],   # sau này có thể giới hạn domain
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.get("/")
-def health_check():
-    return {"status": "ok"}
+app = FastAPI()
 
 @app.post("/ocr")
 async def ocr_endpoint(file: UploadFile = File(...)):
-    if not file.content_type.startswith("image/"):
-        raise HTTPException(status_code=400, detail="File must be an image")
-
     image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
 
-    try:
-        result = run_ocr(image_bytes)
-        return result
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    text = run_ocr(image)
+
+    return {
+        "text": text
+    }
