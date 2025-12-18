5f9fb917883cd80af405db4365041e384eb6bc7a
gamefortoppo <gamefortoppo01@gmail.com>
2025-12-18 08:45:38 +0700

    Update main.py
    support language

--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI, UploadFile, File
-from app.ocr_service import run_ocr
+from fastapi import FastAPI, UploadFile, File, Query
+from app.ocr_service import run_ocr, resolve_language
 from PIL import Image
 import io
 
 app = FastAPI()
 
 @app.post("/ocr")
-async def ocr_endpoint(file: UploadFile = File(...)):
+async def ocr_endpoint(
+    file: UploadFile = File(...),
+    language: str | None = Query(
+        default=None,
+        description="en, vi, ja or en,ja"
+    )
+):
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-    return run_ocr(image)
+
+    lang = resolve_language(language)
+    result = run_ocr(image, lang)
+
+    return {
+        "language_used": lang,
+        **result
+    }
